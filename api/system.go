d35831ab8eb6a3308f99593d160b6779324903d6
ShourGG <sunluming8@gmail.com>
2026-04-08 10:22:07 +0800

    feat: v1.3.1 - add release announcement in upgrade dialog, fix version badge

--- api/system.go
+++ api/system.go
@@ -611,7 +611,7 @@ func getDiskInfo() ([]gin.H, float64) {
 }
 
 func getPanelVersion() string {
-	return "1.3.0"
+	return "1.3.1"
 }
 
 // SelfUpgrade handles POST /system/upgrade
---
+++ frontend/dist/assets/system-DxJZa6iG.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index-CyDftY0z.js";function e(){return t.get("/system/info")}function n(){return t.get("/system/cpu")}function s(){return t.get("/system/memory")}export{n as a,s as b,e as g};
