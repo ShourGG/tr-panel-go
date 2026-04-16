67bd34ab5e38875653667e9912ba463e6b34e6c1
ShourGG <sunluming8@gmail.com>
2026-04-16 22:59:02 +0800

    chore: release v1.3.15

---
+++ web/dist/assets/settings-2-CFlQqm0_.js
@@ -0,0 +1,13 @@
+import{c as e}from"./index-BWwuKvHx.js";
+/**
+ * @license lucide-vue-next v0.364.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("FileCodeIcon",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"m10 13-2 2 2 2",key:"17smn8"}],["path",{d:"m14 17 2-2-2-2",key:"14mezr"}]]),c=e("Settings2Icon",[["path",{d:"M20 7h-9",key:"3s1dr2"}],["path",{d:"M14 17H5",key:"gfn3mx"}],["circle",{cx:"17",cy:"17",r:"3",key:"18b49y"}],["circle",{cx:"7",cy:"7",r:"3",key:"dfmy0x"}]]);
+/**
+ * @license lucide-vue-next v0.364.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */export{a as F,c as S};
