069de099d1c6a2c19a3e944410bbeb26e12413cd
ShourGG <sunluming8@gmail.com>
2026-04-17 17:40:07 +0800

    chore: bump version to v1.3.20-dev.1

--- api/system.go
+++ api/system.go
@@ -662,7 +662,7 @@ func getDiskInfo() ([]gin.H, float64) {
 }
 
 func getPanelVersion() string {
-	return "1.3.18-dev.11"
+	return "1.3.20-dev.1"
 }
 
 func normalizeUpdateChannel(channel string) string {
