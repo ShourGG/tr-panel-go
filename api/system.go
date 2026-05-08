54c2f8bf14fa4376feb1a672a5a973ddf9389e9e
ShourGG <sunluming8@gmail.com>
2026-05-08 23:08:16 +0800

    fix: encode backup ids for restore actions

--- api/system.go
+++ api/system.go
@@ -662,7 +662,7 @@ func getDiskInfo() ([]gin.H, float64) {
 }
 
 func getPanelVersion() string {
-	return "1.5.1-dev.4"
+	return "1.5.1-dev.5"
 }
 
 func normalizeUpdateChannel(channel string) string {
