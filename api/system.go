8daa70b4a441381ab26f3a8a1e84d14fb7fb5080
ShourGG <sunluming8@gmail.com>
2026-05-08 18:16:15 +0800

    chore: prepare v1.5.1-dev.2 build

--- api/system.go
+++ api/system.go
@@ -662,7 +662,7 @@ func getDiskInfo() ([]gin.H, float64) {
 }
 
 func getPanelVersion() string {
-	return "1.5.1-dev.1"
+	return "1.5.1-dev.2"
 }
 
 func normalizeUpdateChannel(channel string) string {
