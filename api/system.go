4d52a642e6f4c9e2dc88ba48381c4e7e054e4c6d
ShourGG <sunluming8@gmail.com>
2026-05-08 18:31:05 +0800

    fix: keep restore dialog stable during analysis

--- api/system.go
+++ api/system.go
@@ -662,7 +662,7 @@ func getDiskInfo() ([]gin.H, float64) {
 }
 
 func getPanelVersion() string {
-	return "1.5.1-dev.2"
+	return "1.5.1-dev.3"
 }
 
 func normalizeUpdateChannel(channel string) string {
